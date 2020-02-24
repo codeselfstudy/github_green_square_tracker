d1700dbc1fb5bbb3f1e0d4f4e8fe4fbe0eb87cc0
J. Cohen <hostelmarketing@gmail.com>
2020-02-24 13:11:23 -0800

    Update App

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,23 @@
 <script>
-    import GithubUserForm from "./components/GithubUserForm.svelte";
     import ActivityTable from "./components/ActivityTable.svelte";
     // export let name;
 </script>
 
 <style>
+
 </style>
 
 <main>
     <h1 class="title is-1">Github Green Square Tracker</h1>
-    <GithubUserForm />
+    <table>
+        <tr>
+            <th>Date</th>
+            <th>Contributions</th>
+        </tr>
+        <tr>
+            <td>2020-02-24</td>
+            <td>100</td>
+        </tr>
+    </table>
     <ActivityTable />
 </main>
