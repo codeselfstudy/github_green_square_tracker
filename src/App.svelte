bb95556bddae0ce9836039a982b4c9cf35a91e5f
J. Cohen <hostelmarketing@gmail.com>
2020-02-24 13:25:13 -0800

    Wire up an event to fetch events

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,15 @@
 <script>
     import ActivityTable from "./components/ActivityTable.svelte";
 
-    let username = "";
-    function createUrl(username) {
-        return `https://api.github.com/users/${username}/events`;
+    export let username = "";
+
+    function createUrl(uname) {
+        return `https://api.github.com/users/${uname}/events`;
     }
 
-    function fetchActivity() {
-        let url = createUrl("j127");
+    function fetchActivity(e) {
+        e.preventDefault();
+        let url = createUrl(username);
         fetch(url)
             .then(res => res.json())
             .then(json => console.table(json));
@@ -22,8 +24,8 @@
     <h1 class="title is-1">Github Green Square Tracker</h1>
     <form>
         <label for="username">Github Username</label>
-        <input type="text" name="username" value={username}>
-        <input type="submit" on:click="fetchActivity" name="submit" value="Fetch Activity">
+        <input type="text" name="username" bind:value={username}>
+        <button type="submit" on:click={fetchActivity}>Fetch Activity</button>
     </form>
     <ActivityTable />
 </main>
