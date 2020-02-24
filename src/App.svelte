a9584bd0b2b92704eba58043cf2c1b8ef31fa32a
J. Cohen <hostelmarketing@gmail.com>
2020-02-24 13:19:15 -0800

    save

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,17 @@
 <script>
     import ActivityTable from "./components/ActivityTable.svelte";
-    // export let name;
+
+    let username = "";
+    function createUrl(username) {
+        return `https://api.github.com/users/${username}/events`;
+    }
+
+    function fetchActivity() {
+        let url = createUrl("j127");
+        fetch(url)
+            .then(res => res.json())
+            .then(json => console.table(json));
+    }
 </script>
 
 <style>
@@ -9,15 +20,10 @@
 
 <main>
     <h1 class="title is-1">Github Green Square Tracker</h1>
-    <table>
-        <tr>
-            <th>Date</th>
-            <th>Contributions</th>
-        </tr>
-        <tr>
-            <td>2020-02-24</td>
-            <td>100</td>
-        </tr>
-    </table>
+    <form>
+        <label for="username">Github Username</label>
+        <input type="text" name="username" value={username}>
+        <input type="submit" on:click="fetchActivity" name="submit" value="Fetch Activity">
+    </form>
     <ActivityTable />
 </main>
