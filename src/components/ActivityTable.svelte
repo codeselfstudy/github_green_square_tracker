f75b3a9e7872efd0f904ba9f6d5a5e61ea3c67d3
J. Cohen <hostelmarketing@gmail.com>
2020-02-24 13:10:13 -0800

    Cleanup and sketches

---
+++ src/components/ActivityTable.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<style>
+.component {
+    outline: #f00 dashed 1px;
+}
+</style>
+
+<div class="component">
+<table>
+    <tr>
+        <th>Date</th>
+        <th>Contributions</th>
+    </tr>
+    <tr>
+        <td>2020-02-24</td>
+        <td>100</td>
+    </tr>
+</table>
+</div>
---
+++ src/components/GithubUserForm.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<style>
+    .component {
+        outline: 1px dashed blue;
+    }
+</style>
+
+<div class="component">
+    <form>
+        <label for="username">Github username</label>
+        <input type="text" name="username" placeholder="Github user name" />
+    </form>
+</div>
